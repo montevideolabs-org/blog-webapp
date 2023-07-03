25778fdb28a89b8f996e92fe0795ffee2abaffab
Juan Mezzera <juan.a.mezzera@gmail.com>
2023-07-03 12:59:36 -0300

    Updated CDK code

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,14 +4,14 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new CdkStack(app, 'BlogPost', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+
+const BASE_DOMAIN = '<YOUR_DOMAIN_HERE>';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'website-bucket', {
+      encryption: s3.BucketEncryption.S3_MANAGED
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hostedZone = route53.HostedZone.fromLookup(this, 'hosted-zone', {
+      domainName: BASE_DOMAIN,
+    });
+
+    const certificate = new acm.DnsValidatedCertificate(this, 'certificate', {
+      domainName: BASE_DOMAIN,
+      hostedZone: hostedZone,
+      region: 'us-east-1',
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'website-distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      domainNames: [BASE_DOMAIN],
+      defaultRootObject: 'index.html',
+      certificate: certificate,
+    });
+
+    const DNSRecord = new route53.ARecord(this, 'DNSRecord', {
+      zone: hostedZone,
+      recordName: BASE_DOMAIN,
+      target: route53.RecordTarget.fromAlias(new route53Targets.CloudFrontTarget(distribution)),
+    })
   }
 }
